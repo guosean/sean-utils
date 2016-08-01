90d4390f6ac25ce4a7270018687513a1505b644d
郭振宾 <zhenbin.guo@qunar.com>
2016-08-01 23:01:00 +0800

    pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,16 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by guozhenbin on 16/8/1.
  */
 public class Main {
 
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         System.out.println("test");
+        logger.info("info");
     }
 
 }
