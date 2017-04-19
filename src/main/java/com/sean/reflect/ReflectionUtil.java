6a7458769939ed326a802d8d56c5495fd08dbb5d
郭振宾 <zhenbin.guo@qunar.com>
2017-04-19 23:33:37 +0800

    a

--- src/main/java/com/sean/reflect/ReflectionUtil.java
+++ src/main/java/com/sean/reflect/ReflectionUtil.java
@@ -1,9 +1,13 @@
 package com.sean.reflect;
 
+import com.alibaba.dubbo.common.utils.CollectionUtils;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.MethodUtils;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 /**
  * Created by guozhenbin on 2017/3/3.
@@ -19,27 +23,70 @@ public class ReflectionUtil {
             return null;
         }
         Object value = null;
-        try {
-            Field field = object.getClass().getDeclaredField(fieldName);
-            String methodPrefix = isBooleanType(field) ? PREFIX_IS : PREFIX_GET;
-            String methodName = String.format("%s%s", methodPrefix, StringUtils.capitalize(fieldName));
-            value = MethodUtils.invokeMethod(object, methodName, null);
-        } catch (Exception e) {
-        }
+        Class<?> targetClzz = object.getClass();
+        do{
+            try {
+                Field field = targetClzz.getDeclaredField(fieldName);
+                String methodPrefix = isBooleanType(field) ? PREFIX_IS : PREFIX_GET;
+                String methodName = String.format("%s%s", methodPrefix, StringUtils.capitalize(fieldName));
+                value = MethodUtils.invokeMethod(object, methodName, null);
+            } catch (Exception e) {
+                continue;
+            } finally {
+                targetClzz = targetClzz.getSuperclass();
+            }
+        }while(null!=targetClzz && targetClzz!=Object.class);
 
         return value;
     }
 
     public static void invokeSetterMethod(Object object, String field, Object value) {
-        if (null == object || StringUtils.isBlank(field)) {
+        if (null == object || StringUtils.isBlank(field) || null == value) {
             return;
         }
 
         String methodName = String.format("set%s", StringUtils.capitalize(field));
         try {
             MethodUtils.invokeMethod(object, methodName, value);
-        } catch (Exception e) {
+        } catch (Exception e) {}
+    }
+
+    public static List<String> getFieldNames(Class clzz){
+        Preconditions.checkNotNull(clzz,"clzz cannot be null");
+        List<String> fieldNames = Lists.newArrayList();
+
+        Class<?> targetClass = clzz;
+        do {
+            Field[] fields = targetClass.getDeclaredFields();
+            for (Field field : fields) {
+                fieldNames.add(field.getName());
+            }
+            targetClass = targetClass.getSuperclass();
         }
+        while (targetClass != null && targetClass != Object.class);
+
+        return fieldNames;
+    }
+
+    /**
+     * 将srcObject中的filedValue set到targetObject对应字段
+     * @param srcObject
+     * @param targetObject
+     */
+    public static void cloneObject(Object srcObject,Object targetObject){
+        Preconditions.checkNotNull(srcObject,"srcObject cannot not be null");
+        Preconditions.checkNotNull(targetObject,"targetObject cannot be null");
+        Class srcClzz = srcObject.getClass();
+        List<String> srcFileds = getFieldNames(srcClzz);
+        if(CollectionUtils.isNotEmpty(srcFileds)){
+            for(String field:srcFileds){
+                try {
+                    Object fieldValue = invokeGetterMethod(srcObject,field);
+                    invokeSetterMethod(targetObject,field,fieldValue);
+                } catch (Exception e) {}
+            }
+        }
+
     }
 
     private static boolean isBooleanType(Field field){
@@ -47,7 +94,6 @@ public class ReflectionUtil {
             return false;
         }
         String typeName = field.getName();
-        //考虑基础类型boolean 和 Boolean 两种不同的type
         return StringUtils.equals(Boolean.class.getName(),typeName) || StringUtils.equals(TYPE_BOOLEAN,typeName);
     }
 
